9a4cdcea8589ceb7ef4fbc5b6d7e7a123d361181
Anniiia <annaortegabueno@gmail.com>
2024-04-18 20:45:38 +0200

    Add project files.

---
+++ AppAccionesWebJob/Data/AccionContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProyectoNetCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppAccionesWebJob.Data
+{
+    public class AccionContext: DbContext
+    {
+        public AccionContext(DbContextOptions<AccionContext> options)
+            : base(options) { }
+
+        public DbSet<Accion> Acciones { get; set; }
+
+    }
+}
---
+++ AppAccionesWebJob/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AppAccionesWebJob.Data;
+using AppAccionesWebJob.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+Console.WriteLine("Bienvenido a Acciones");
+
+string connectionString =
+    @"Data Source=sqlanniie.database.windows.net;Initial Catalog=AZURETAJAMAR;Persist Security Info=True;User ID=adminsql;Password=Admin123;Trust Server Certificate=True";
+
+//NECESITAMOS UTILIZAR INYECCION DE DEPENDENCIAS
+//PARA ELLO DEBEMOS CREAR UN PROVIDER
+var provider = new ServiceCollection()
+    .AddTransient<RepositoryAcciones>()
+    .AddDbContext<AccionContext>
+    (options => options.UseSqlServer(connectionString))
+    .BuildServiceProvider();
+
+//MEDIANTE EL PROVEEDOR, YA PODEMOS RECUPERAR NUESTRO REPOSITORIO
+//PARA EJECUTAR EL METODO DE Populate
+RepositoryAcciones repo = provider.GetService<RepositoryAcciones>();
+Console.WriteLine("Pulse ENTER para iniciar...");
+Console.ReadLine();
+await repo.InsertarAccionDia();
+Console.WriteLine("Proceso completado correctamente");
+Console.WriteLine("Fin de las acciones");
+
