c71d7648fa559e5a80b52c9709deabef393b73d5
Anniiia <annaortegabueno@gmail.com>
2024-04-19 17:10:08 +0200

    commit

--- AppAccionesWebJob/Program.cs
+++ AppAccionesWebJob/Program.cs
@@ -4,7 +4,6 @@ using AppAccionesWebJob.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
-Console.WriteLine("Bienvenido a Acciones");
 
 string connectionString =
     @"Data Source=sqlanniie.database.windows.net;Initial Catalog=AZURETAJAMAR;Persist Security Info=True;User ID=adminsql;Password=Admin123;Trust Server Certificate=True";
@@ -20,9 +19,7 @@ var provider = new ServiceCollection()
 //MEDIANTE EL PROVEEDOR, YA PODEMOS RECUPERAR NUESTRO REPOSITORIO
 //PARA EJECUTAR EL METODO DE Populate
 RepositoryAcciones repo = provider.GetService<RepositoryAcciones>();
-Console.WriteLine("Pulse ENTER para iniciar...");
-Console.ReadLine();
+
 await repo.InsertarAccionDia();
-Console.WriteLine("Proceso completado correctamente");
-Console.WriteLine("Fin de las acciones");
+
 
